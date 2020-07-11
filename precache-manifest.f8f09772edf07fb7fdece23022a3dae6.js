6cda98ba79c84c6c941e9fdf51f18ca0d4fec894
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-07-10 23:47:52 -0500

    Updates

---
+++ precache-manifest.f8f09772edf07fb7fdece23022a3dae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d285808c28d79f082b94d4eb0d3046",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "76c4311664225ced8380",
+    "url": "/react-portfolio/static/css/main.e30e77c9.chunk.css"
+  },
+  {
+    "revision": "a6a855948a1a9aa382b7",
+    "url": "/react-portfolio/static/js/2.03e60049.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.03e60049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c4311664225ced8380",
+    "url": "/react-portfolio/static/js/main.382b21e5.chunk.js"
+  },
+  {
+    "revision": "a05a49d9351ba3961537",
+    "url": "/react-portfolio/static/js/runtime-main.bc59954b.js"
+  }
+]);
